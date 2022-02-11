60634931fa9c3f2853f95354f74a445b1300fc40
Sergey Zolotarev <sz@damoov.com>
2022-02-12 02:34:49 +0300

    add test layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngrx-test';
+  count: number = 1;
+  public get cannotDecrese(): boolean {
+    return this.count <= 0;
+  }
+  lastUpdate?: number;
+
+  runTimer(): void {
+    setInterval(() => (this.count += 0.00364583333), 10);
+  }
+
+  increment(): void {
+    this.lastUpdate = Date.now();
+    this.count++;
+  }
+
+  decrement(): void {
+    this.lastUpdate = Date.now();
+    this.count--;
+  }
+
+  reset(): void {
+    this.lastUpdate = Date.now();
+    this.count = 0;
+  }
 }
